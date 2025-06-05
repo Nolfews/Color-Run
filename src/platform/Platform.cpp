e332e8ede0fe033139d6727c01a634dc23a34b39
Matisse-M <matisse.marsac@epitech.eu>
2025-06-05 22:06:07 +0200

    zim

---
+++ src/platform/Platform.cpp
@@ -0,0 +1,16 @@
+/*
+** EPITECH PROJECT, 2025
+** Color-Run
+** File description:
+** Platform
+*/
+
+#include "Platform.hpp"
+
+Platform::Platform()
+{
+}
+
+Platform::~Platform()
+{
+}
---
+++ src/platform/Platform.hpp
@@ -0,0 +1,20 @@
+/*
+** EPITECH PROJECT, 2025
+** Platform
+** File description:
+** Platform
+*/
+
+#ifndef PLATFORM_HPP_
+    #define PLATFORM_HPP_
+
+class Platform {
+    public:
+        Platform();
+        ~Platform();
+
+    protected:
+    private:
+};
+
+#endif /* !PLATFORM_HPP_ */
