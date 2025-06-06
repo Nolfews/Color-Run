8e1c43dfc712da4b7b4a82732494aca1cd9e14ca
marinlamy <marin.lamy@epitech.eu>
2025-06-06 18:03:01 +0200

    le menu + mort

--- src/game/game.hpp
+++ src/game/game.hpp
@@ -22,6 +22,43 @@
 #define CIRCLE_SPACING 50.0f
 #define INDICATOR_Y_POSITION 30.0f
 
+enum class GameState {
+    MENU,
+    PLAYING,
+    SETTINGS,
+    GAME_OVER
+};
+
+enum class MenuOption {
+    PLAY = 0,
+    SETTINGS = 1,
+    QUIT = 2
+};
+
+enum class SettingsOption {
+    CONTROLS = 0,
+    SCREEN_SIZE = 1,
+    BACK = 2
+};
+
+enum class KeyBindingOption {
+    LEFT = 0,
+    RIGHT = 1,
+    UP = 2,
+    DOWN = 3,
+    TAB = 4,
+    RESTART = 5,
+    BACK = 6
+};
+
+enum class ScreenSizeOption {
+    SIZE_1280x720 = 0,
+    SIZE_1920x1080 = 1,
+    SIZE_2560x1440 = 2,
+    FULLSCREEN = 3,
+    BACK = 4
+};
+
 class Game {
 public:
     Game();
@@ -49,6 +86,35 @@ private:
     void renderColorOverlay();
     void updateCamera();
     void updateLivesDisplay();
+
+    void handleMenuKeyPressed(sf::Keyboard::Key key);
+    void renderMenu();
+    void initMenu();
+    void navigateMenu(int direction);
+    void selectMenuOption();
+    void startGame();
+    void openSettings();
+    void quitGame();
+    void handleSettingsKeyPressed(sf::Keyboard::Key key);
+    void renderSettings();
+    void initSettings();
+    void navigateSettings(int direction);
+    void selectSettingsOption();
+    void renderKeyBindingMenu();
+    void handleKeyBindingKeyPressed(sf::Keyboard::Key key);
+    void navigateKeyBinding(int direction);
+    void selectKeyBindingOption();
+    void renderScreenSizeMenu();
+    void handleScreenSizeKeyPressed(sf::Keyboard::Key key);
+    void navigateScreenSize(int direction);
+    void selectScreenSizeOption();
+    void changeWindowSize(int width, int height, bool fullscreen = false);
+    void resetToDefaultKeys();
+    void backToMainMenu();
+    void backToSettings();
+    void updateKeyBindingDisplay();
+    std::string getKeyName(sf::Keyboard::Key key);
+    void recreateGameEntities();
     void checkPlayerEnemyCollision();
     void resetPlayerToSpawn();
     void handlePlayerDeath();
@@ -86,6 +152,30 @@ private:
     sf::Text _gameOverText;
     sf::Text _finalScoreText;
     sf::Text _scoreText;
+
+    GameState _gameState;
+    MenuOption _currentMenuOption;
+    sf::Texture _titleTexture;
+    sf::Sprite _titleSprite;
+    std::vector<sf::Text> _menuOptions;
+    sf::RectangleShape _menuSelector;
+
+    SettingsOption _currentSettingsOption;
+    KeyBindingOption _currentKeyBindingOption;
+    ScreenSizeOption _currentScreenSizeOption;
+    std::vector<sf::Text> _settingsOptions;
+    std::vector<sf::Text> _keyBindingOptions;
+    std::vector<sf::Text> _screenSizeOptions;
+    bool _isInKeyBinding;
+    bool _isInScreenSize;
+    bool _fullscreen;
+    sf::Vector2u _windowedSize;
+    sf::Keyboard::Key _leftKey;
+    sf::Keyboard::Key _rightKey;
+    sf::Keyboard::Key _upKey;
+    sf::Keyboard::Key _downKey;
+    sf::Keyboard::Key _tabKey;
+    sf::Keyboard::Key _restartKey;
 };
 
 
